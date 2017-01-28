1aae7901dc26cfa210551eb37d262490aac7eeb5
ctang003 <ctang003@gmail.com>
2017-01-28 13:35:40 +0800

    加上 ctang003 is in. 在 stringFromJNI 裡,我還是習慣用C++

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -6,6 +6,6 @@ jstring
 Java_tw_com_everydayenergy_everydayenergy_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello from C++. ctang003 is in.";
     return env->NewStringUTF(hello.c_str());
 }
